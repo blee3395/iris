201259141eb4fec151ccf31e80cbbff9caa3e4c0
Brennan <blee3395@gmail.com>
2022-08-20 10:26:36 -0400

    main save

--- producer/index.js
+++ producer/index.js
@@ -5,7 +5,7 @@ const eventInteger = require('../eventTypes/eventInteger.js')
 
 const kafka = new Kafka({
   clientId: 'iris',
-  brokers: ['localhost:9092'],
+  brokers: ['ec2-34-235-115-252.compute-1.amazonaws.com:9092'],
   requestTimeout: 25000,
   connectionTimeout: 3000,
   retry: {
